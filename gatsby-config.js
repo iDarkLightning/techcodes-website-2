159bf972321d15b6aacb7694f8c55bc8c1b2d2c8
Nirjhor Nath <nirjhor.nath@gmail.com>
2021-10-24 17:38:00 -0400

    setup
    Co-authored-by: kyryloren <dev@kyryloorlov.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,28 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `TechCodes`,
+    description: `TechCodes is a student-led nonprofit organization dedicated to fostering computer science education to students of all backgrounds.`,
+    siteUrl: `https://techcodes.org`,
+    image: "/og.png",
+    twitterUsername: "@bthscode",
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `@chakra-ui/gatsby-plugin`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: process.env.DATOCMS_TOKEN,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -15,24 +30,24 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`DM sans\:400,400i,500,500i,700,700i`],
+        display: "swap",
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `techcodes`,
+        short_name: `techcodes`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        background_color: `#1D166F`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
-}
+};
---
+++ src/@chakra-ui/gatsby-plugin/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = {
+  colors: {
+    text: "#F6F6F6",
+    background: "#1D166F",
+    accent: "#E7E7E7",
+    yellow: "#FFCC66",
+    undertone: "#161616",
+    gray: "#969696",
+    pink: "#FF8FE6",
+  },
+};
+
+export default extendTheme(theme);
